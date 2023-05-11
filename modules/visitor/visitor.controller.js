8e85a932c77b5e6e599d1ad028ab08536a9eb4ff
rash <orashusedmund@gmail.com>
2023-05-12 00:56:21 +0100

    modified the cors config

--- modules/visitor/visitor.controller.js
+++ modules/visitor/visitor.controller.js
@@ -45,7 +45,7 @@ class VisitorController {
     const ipgeolocationApi = new IPGeolocationAPI(IPGEOLOCATION_API_KEY, false);
 
     const geolocationParams = new GeolocationParams();
-    geolocationParams.setIPAddress(ip_address);
+    geolocationParams.setIPAddress("154.72.160.145" || ip_address);
 
     ipgeolocationApi.getGeolocation((json) => { 
       console.log(2)
